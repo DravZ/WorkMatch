6a181d935d8726f5fd0c64a9d254040ab3ab1996
DravZ <cdgo345@gmail.com>
2026-08-17 21:05:29 -0600

    feat(front/worker/dashboard): add findJobs page

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -8,13 +8,13 @@ import GuestLayout from './layouts/GuestLayout/GuestLayout';
 import Login from './pages/guest/Login/Login';
 import Register from './pages/guest/Register/Register';
 import Applications from './pages/worker/Applications/Applications';
-import FindJobs from './pages/worker/FindJobs/FindJobs';
 import Messages from './pages/worker/Messages/Messages';
 import WorkerProfile from './pages/worker/Profile/WorkerProfile';
 import Saved from './pages/worker/Saved/Saved';
 import WorkerLayout from './layouts/WorkerLayout/WorkerLayout';
 import WorkerNotifications from './pages/worker/Notifications/WorkerNotifications';
 import { WorkerDashboard } from './pages/worker/Dashboard/WorkerDashboard';
+import { FindJobs } from './pages/worker/FindJobs/FindJobs';
 
 function App() {
   return (
