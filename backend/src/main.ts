73cb0c6f66a90ba4fb2bd32c7f4e425fa56154af
DravZ <cdgo345@gmail.com>
2026-08-20 13:14:32 -0600

    feat(back): Add swagger plugin

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,38 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('WorkMatch API')
+    .setDescription('Documentación de la API de WorkMatch')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        description: 'Ingresa el token JWT',
+      },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
+
+  console.log(
+    `🚀 API disponible en: http://localhost:${process.env.PORT ?? 3000}`,
+  );
+  console.log(
+    `📚 Swagger disponible en: http://localhost:${process.env.PORT ?? 3000}/api`,
+  );
 }
+
 bootstrap();
